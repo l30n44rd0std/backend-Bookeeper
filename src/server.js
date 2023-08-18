ce7e1334c7377b309ed2a176dae58d0f2899154d
l30n44rd0 <ljsf2@aluno.ifal.edu.br>
2023-08-18 12:38:30 -0300

    mudando porta e force para false

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ const db = require('./db/connect');
 const routers = require("./routes/index")
 
 const app = express()
+
+
 app.use(express.json());
-const port = 3000
+const port = 3005
 
 app.use(routers)
 
 
 app.listen(port, async () => {
-     await db.sync({force:true});
+     await db.sync({force:false});
     console.log('server started');
 })
