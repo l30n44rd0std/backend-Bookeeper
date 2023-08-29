6ce6b1fe12f1ec544df6213ccc73bd6419d71a56
l30n44rd0 <ljsf2@aluno.ifal.edu.br>
2023-08-29 11:49:12 -0300

    add middleware com autenticação de usuário

---
+++ src/middleware/AuthenticationUser.js
@@ -0,0 +1,41 @@
+const { sign, verify } = require("jsonwebtoken")
+
+function authenticationUser({ email, password }) {
+    try {
+
+        const token = sign(
+            {
+                email: email,
+                password: password,
+            },
+            "8ee7ed39-c9b5-49eb-95cf-cd0936ea172b",  
+           
+        );
+        console.log(token);
+        return {email: email, token: token};
+    } catch (err) {
+        throw new Error('error generetor token');
+    }
+}
+
+function ensureAuthenticated(req, res, next) {
+    const tk = req.headers.authorization;
+    if (!tk) {
+        return res.status(401).json({
+            message: "token not found",
+        });
+    }
+    const [, token] = tk.split(" ");
+    console.log(token);
+    try {
+        verify(token, process.env.SECRET_JWT);
+        next();
+    } catch (err) {
+        return res.status(401).json({
+            message: 'invalid token',
+        });
+    }
+}
+
+module.exports = { ensureAuthenticated, authenticationUser }
+
